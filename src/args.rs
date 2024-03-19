fe48df8b83b4be891e6e5005369148e586b555f2
prokhor40 <prohornikitin@gmail.com>
2024-03-19 23:32:17 +0300

    set default threads number as number of logical cpu cores

--- src/args.rs
+++ src/args.rs
@@ -1,33 +1,57 @@
 use clap::Parser;
+use clap_num::number_range;
 
-#[derive(Parser, Debug)]
-pub struct Args {
+fn from_1_to_32(s: &str) -> Result<u8, String> {
+    number_range(s, 1, 32)
+}
+
+#[derive(Parser)]
+struct RawArgs {
     #[arg(
         short='N',
         default_value="1",
+        value_parser=from_1_to_32,
         help="number of trailing zeros which hash should have"
     )]
-    pub trailing_zeros: usize,
+    trailing_zeros: u8,
 
     #[arg(
         short='F',
         default_value="1",
-        help="number of hashes to generated"
+        help="number of hashes to find"
     )]
-    pub hashes_needed: usize,
+    hashes_needed: usize,
 
     #[arg(
         short='j', 
         long="threads",
-        default_value="4",
-        help="number of threads"
+        help="number of threads [default: number of cpu logical cores]"
     )]
-    pub threads: usize,
+    threads: Option<usize>,
 
     #[arg(
         long="chunk-size",
         default_value="4096",
         help="how many numbers process on each thread for one job"
     )]
+    chunk_size: usize,
+}
+
+pub struct Args {
+    pub trailing_zeros: u8,
+    pub hashes_needed: usize,
+    pub threads: usize,
     pub chunk_size: usize,
+}
+
+impl Args {
+    pub fn parse() -> Args {
+        let raw = RawArgs::parse();
+        return Args {
+            trailing_zeros: raw.trailing_zeros,
+            hashes_needed: raw.hashes_needed,
+            threads: raw.threads.unwrap_or(num_cpus::get()),
+            chunk_size: raw.chunk_size,
+        }
+    }
 }
