5d2976766eb960402d039f99a20dd0f805579181
prokhor40 <prohornikitin@gmail.com>
2024-03-19 20:22:55 +0300

    thread_pool works

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,160 @@
-use std::{ops::Range, thread::{self, JoinHandle}};
+use std::{ops::Range, sync::mpsc::{self, Sender}};
 
 use regex::Regex;
 use sha256;
 use clap::Parser;
+use threadpool::ThreadPool;
 
 #[derive(Parser, Debug)]
 // #[command(0.1, Command, long_about = None)]
 struct Args {
     /// Name of the person to greet
     #[arg(short='N', default_value="1", help="number of trailing zeros which hash should have")]
-    trailing_zeros: u64,
+    trailing_zeros: usize,
 
     /// Number of times to greet
     #[arg(short='F', default_value="1", help="number of hashes to generated")]
-    hashes_needed: u64,
+    hashes_needed: usize,
 }
 
 
-fn has_exatly_n_trailing_zeros(n: u64) -> Regex {
+fn has_exatly_n_trailing_zeros(n: usize) -> Regex {
     Regex::new(format!(r"0{{{}}}$", n).as_str()).unwrap()
 }
 
 
 #[derive(Clone, Debug)]
-struct Variant {
-    input: u64,
+struct HashCase {
+    input: usize,
     digest: String,
 }
 
-fn spawn_worker_thread(inputs: Range<u64>, regex: Regex) -> JoinHandle<Vec<Variant>> {
-    thread::spawn(move|| {
-        let mut results: Vec<Variant> = vec![];
-        for i in inputs {
-            let digest = sha256::digest(i.to_string());
-            if regex.is_match(digest.as_str()) {
-                results.push(Variant {
-                    input: i,
-                    digest: digest,
-                });
-            }
+
+fn find_cases_by_digest_regex(
+    inputs: Range<usize>,
+    needed_digest: Regex
+) -> Vec<HashCase> {
+    let mut results: Vec<HashCase> = Vec::new(); 
+    for i in inputs {
+        let digest = sha256::digest(i.to_string());
+        if !needed_digest.is_match(digest.as_str()) {
+            continue;
         }
-        return results;
-    })
+        results.push(HashCase {
+            input: i,
+            digest: digest,
+        });
+    }
+    return results;
 }
 
-const THREADS: u64 = 4;
-const CHUNK_SIZE: u64 = 1024*256;
+
+
+struct TaskChunkInput {
+    inputs: Range<usize>,
+    needed_digest: Regex
+}
+
+impl TaskChunkInput {
+    fn new(inputs: Range<usize>, needed_digest: &Regex) -> TaskChunkInput{
+        TaskChunkInput {
+            inputs,
+            needed_digest: needed_digest.clone(),
+        }
+    }
+}
+
+fn schedule_task_chunk(
+    pool: &ThreadPool,
+    output: &Sender<Vec<HashCase>>,
+    task: TaskChunkInput,
+) {
+    let output_clone = output.clone();
+    pool.execute(move|| {
+        output_clone.send(
+            find_cases_by_digest_regex(task.inputs, task.needed_digest)
+        ).unwrap();
+    });
+}
+
+struct ChunksBoundsIterator {
+    chunk_size: usize,
+    next_chunk_start: usize,
+}
+
+impl ChunksBoundsIterator {
+    fn new(chunk_size: usize, start: usize) -> ChunksBoundsIterator {
+        return ChunksBoundsIterator {
+            chunk_size,
+            next_chunk_start: start,
+        }
+    }
+}
+
+impl Iterator for ChunksBoundsIterator {
+    type Item = Range<usize>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let start = self.next_chunk_start;
+        let next_start_or_overflow = start.checked_add(self.chunk_size);
+        if next_start_or_overflow.is_none() {
+            return None;
+        }
+        let next_start = next_start_or_overflow.unwrap(); 
+        self.next_chunk_start = next_start;
+        return Some(start..next_start);
+    }
+}
+
+
+const CHUNK_SIZE: usize = 1024;
 fn main() {
     let args = Args::parse();
+    let pool = ThreadPool::new(4);
+    let (sender, receiver) = mpsc::channel::<Vec<HashCase>>();
 
-    let mut new_chunk_start = 1;
-    let mut found = 0;
     let regex = has_exatly_n_trailing_zeros(args.trailing_zeros);
-    while new_chunk_start < 18446744073709551615 && found < args.hashes_needed  {
-        let mut handles: Vec<JoinHandle<Vec<Variant>>> = vec![];
-        for _ in 1..THREADS {
-            handles.push(spawn_worker_thread(
-                new_chunk_start..new_chunk_start+CHUNK_SIZE,
-                regex.clone()
-            ));
-            new_chunk_start += CHUNK_SIZE;
-        }
-        let mut results: Vec<Variant> = vec![];
-        for h in handles {
-            for v in h.join().unwrap() {
-                results.push(v);
-            }
-        }
-        results.sort_unstable_by_key(|x| x.input);
-        for v in results {
-            println!("{}, {}", v.input, v.digest);
+    let mut task_inputs = ChunksBoundsIterator::new(CHUNK_SIZE, 1)
+        .map(|range| TaskChunkInput::new(range, &regex));
+    
+    for _ in 0..pool.max_count() {
+        let task = task_inputs.next().unwrap();
+        schedule_task_chunk(&pool, &sender, task);    
+    }
+    
+    let mut found = 0;
+    let mut results: Vec<HashCase> = Vec::with_capacity(args.hashes_needed);
+
+    while let Ok(received) = receiver.recv() {
+        for case in received {
+            results.push(case);
             found += 1;
-            if found >= args.hashes_needed {
-                break;
-            }
         }
+
         if found >= args.hashes_needed {
             break;
         }
+        let input = task_inputs.next();
+        if input.is_none() {
+            break;
+        }
+        schedule_task_chunk(&pool, &sender, input.unwrap())
     }
-    
+
+    drop(sender);
+    pool.join();
+
+    while let Ok(received) = receiver.recv() {
+        for case in received {
+            results.push(case);
+            found += 1;
+        }
+    }
+
+
+    results.sort_unstable_by_key(|x| x.input);
+
+    for r in results.iter().take(args.hashes_needed) {
+        println!("{}, {}", r.input, r.digest);
+    }
 }
